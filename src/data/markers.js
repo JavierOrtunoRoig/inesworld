f998437c8e27eaf4bf0286d3c3ce60c26a93f1b5
Javier <javiortunoroig.com>
2024-04-27 23:31:43 +0200

    add markers with planes

---
+++ src/data/markers.js
@@ -0,0 +1,9 @@
+const malaga = [36.72016, -4.42034]
+const seul = [12.879721, 121.774017]
+
+const NotTraveling = []
+
+export const markersToShow = {
+  livingIn: malaga,
+  travellingTo: NotTraveling
+}
