dba1225d0a11fcfd3b27de038681379a6260982c
Javier <javiortunoroig.com>
2024-04-28 15:55:18 +0200

    add tags

--- src/data/markers.js
+++ src/data/markers.js
@@ -1,9 +1,9 @@
 const malaga = [36.72016, -4.42034]
-const seul = [12.879721, 121.774017]
+const seul = [37.532600, 127.024612]
 
 const NotTraveling = []
 
 export const markersToShow = {
   livingIn: malaga,
-  travellingTo: NotTraveling
+  travellingTo: seul
 }
