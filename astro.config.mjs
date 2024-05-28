014e3535b8eabf2d1cc28b411e4ce20aa54eae6e
Javier <javiortunoroig.com>
2024-05-28 23:35:50 +0200

    update sitemap with i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,5 +19,13 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime]
   },
-  integrations: [mdx(), tailwind(), astroI18next(), sitemap()]
+  integrations: [mdx(), tailwind(), astroI18next(), sitemap({
+    i18n: {
+        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+          es: 'es-ES',
+        },
+      },
+  })]
 });
