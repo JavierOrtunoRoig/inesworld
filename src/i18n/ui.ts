15b3407b182ef7b8f88317be4249ea34324f2cc0
Javier <javiortunoroig.com>
2024-05-28 19:04:41 +0200

    add traductions

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,30 +1,40 @@
 export const languages = {
-  en: 'English',
-  es: 'Español',
+  en: "English",
+  es: "Español",
 };
 
 export const isos = {
-  en: 'gb',
-  es: 'es',
+  en: "gb",
+  es: "es",
 };
 
 export const showDefaultLang = false;
-export const defaultLang = 'en';
+export const defaultLang = "en";
 
 export const ui = {
   en: {
+    "navbar.home": "Home",
+    "navbar.about": "About me",
+    "navbar.portfolio": "Portfolio",
+    "navbar.blog": "Blog",
+    "navbar.contact": "Contact",
+    "navbar.trip_map": "Trip Map",
+
     "showreel.title": "My Showreel",
     "showreel.button": "Discover my work!",
 
     "why_me.title": "Why Me?",
     "why_me.reason_1.title": "Inclusive Storytelling",
-    "why_me.reason_1.description": "Authentic visuals that break barriers and contribute to positive social change. I’m unlocking the full potential of your brand by creating a space for all stories to be heard and valued. That sets you apart as a leader of promoting diversity and equality.",
+    "why_me.reason_1.description":
+      "Authentic visuals that break barriers and contribute to positive social change. I’m unlocking the full potential of your brand by creating a space for all stories to be heard and valued. That sets you apart as a leader of promoting diversity and equality.",
 
     "why_me.reason_2.title": "Sustainable Business",
-    "why_me.reason_2.description": "The stories I tell are individually tailored for companies that prioritize sustainability. This will not only help to preserve the earth and cultures for future generations, but also positions your business for long-term success.",
+    "why_me.reason_2.description":
+      "The stories I tell are individually tailored for companies that prioritize sustainability. This will not only help to preserve the earth and cultures for future generations, but also positions your business for long-term success.",
 
     "why_me.reason_3.title": "Embracing Community",
-    "why_me.reason_3.description": "I’m dedicated to creating engaging and relevant content that helps showcasing your brand’s values and builds a strong sense of community around your brand to ensure the success of a business and its mission.",
+    "why_me.reason_3.description":
+      "I’m dedicated to creating engaging and relevant content that helps showcasing your brand’s values and builds a strong sense of community around your brand to ensure the success of a business and its mission.",
 
     "skills.skill_1.title": "SCRIPT WRITING",
     "skills.skill_1.reason": "Vision from the concept and developing",
@@ -37,19 +47,24 @@ export const ui = {
 
     "process.title": "My Process",
     "process.step_1.title": "1. Connect",
-    "process.step_1.description": "Connect with me to share your project details. Together, we'll explore how I can help you achieve your goals and bring your vision to life.",
+    "process.step_1.description":
+      "Connect with me to share your project details. Together, we'll explore how I can help you achieve your goals and bring your vision to life.",
 
     "process.step_2.title": "2. Concept",
-    "process.step_2.description": "We develop unique and creative visual concepts, individually designed for you. We tell meaningful stories and build a strong community around your brand.",
+    "process.step_2.description":
+      "We develop unique and creative visual concepts, individually designed for you. We tell meaningful stories and build a strong community around your brand.",
 
     "process.step_3.title": "3. Create",
-    "process.step_3.description": "Now it's my turn. I start working on your project, using my expertise and creativity to turn your vision into reality.",
+    "process.step_3.description":
+      "Now it's my turn. I start working on your project, using my expertise and creativity to turn your vision into reality.",
 
     "process.step_4.title": "4. Review",
-    "process.step_4.description": "I receive your feedback on the project. I work with you to make the necessary adjustments and ensure the final result meets your expectations.",
+    "process.step_4.description":
+      "I receive your feedback on the project. I work with you to make the necessary adjustments and ensure the final result meets your expectations.",
 
     "process.step_5.title": "5. Deliver",
-    "process.step_5.description": "I deliver the final product, making sure it meets your expectations and is ready to impress your audience.",
+    "process.step_5.description":
+      "I deliver the final product, making sure it meets your expectations and is ready to impress your audience.",
 
     "latest_works.title": "Latest Works",
 
@@ -58,7 +73,8 @@ export const ui = {
     "portfolio.title": "See gallery",
 
     "contact.title": "CONTACT WITH ME!",
-    "contact.subtitle": "Maybe here we can put a text giving thanks to users or any message that Ines want to share",
+    "contact.subtitle":
+      "Maybe here we can put a text giving thanks to users or any message that Ines want to share",
     "contact.label.name": "Name:",
     "contact.label.email": "Email:",
     "contact.label.message": "Message:",
@@ -69,18 +85,28 @@ export const ui = {
     "map.subtitle_2": "Countries visited!!",
   },
   es: {
+    "navbar.home": "Inicio",
+    "navbar.about": "Sobre mí",
+    "navbar.portfolio": "Portafolio",
+    "navbar.blog": "Blog",
+    "navbar.contact": "Contacto",
+    "navbar.trip_map": "Mapa de viajes",
+
     "showreel.title": "Mi showreel",
     "showreel.button": "¡Descubre mi trabajo!",
 
     "why_me.title": "¿Por qué yo?",
     "why_me.reason_1.title": "Narración Inclusiva",
-    "why_me.reason_1.description": "Visuales auténticos que rompen barreras y contribuyen al cambio social positivo. Estoy desbloqueando el potencial completo de tu marca creando un espacio para que todas las historias sean escuchadas y valoradas. Esto te distingue como un líder en la promoción de la diversidad y la igualdad.",
+    "why_me.reason_1.description":
+      "Visuales auténticos que rompen barreras y contribuyen al cambio social positivo. Estoy desbloqueando el potencial completo de tu marca creando un espacio para que todas las historias sean escuchadas y valoradas. Esto te distingue como un líder en la promoción de la diversidad y la igualdad.",
 
     "why_me.reason_2.title": "Negocios Sostenibles",
-    "why_me.reason_2.description": "Las historias que cuento están especialmente diseñadas para empresas que priorizan la sostenibilidad. Esto no solo ayudará a preservar la tierra y las culturas para las futuras generaciones, sino que también posicionará tu negocio para el éxito a largo plazo.",
+    "why_me.reason_2.description":
+      "Las historias que cuento están especialmente diseñadas para empresas que priorizan la sostenibilidad. Esto no solo ayudará a preservar la tierra y las culturas para las futuras generaciones, sino que también posicionará tu negocio para el éxito a largo plazo.",
 
     "why_me.reason_3.title": "Abrazando la Comunidad",
-    "why_me.reason_3.description": "Estoy dedicado a crear contenido atractivo y relevante que ayude a mostrar los valores de tu marca y construir un fuerte sentido de comunidad alrededor de ella para asegurar el éxito del negocio y su misión.",
+    "why_me.reason_3.description":
+      "Estoy dedicado a crear contenido atractivo y relevante que ayude a mostrar los valores de tu marca y construir un fuerte sentido de comunidad alrededor de ella para asegurar el éxito del negocio y su misión.",
 
     "skills.skill_1.title": "ESCRITURA DE GUIONES",
     "skills.skill_1.reason": "Visión desde el concepto y desarrollo",
@@ -93,19 +119,24 @@ export const ui = {
 
     "process.title": "Mi Proceso",
     "process.step_1.title": "1. Conectar",
-    "process.step_1.description": "Conéctate conmigo para compartir los detalles de tu proyecto. Juntos exploraremos cómo puedo ayudarte a alcanzar tus objetivos y dar vida a tu visión.",
+    "process.step_1.description":
+      "Conéctate conmigo para compartir los detalles de tu proyecto. Juntos exploraremos cómo puedo ayudarte a alcanzar tus objetivos y dar vida a tu visión.",
 
     "process.step_2.title": "2. Concepto",
-    "process.step_2.description": "Desarrollamos conceptos visuales únicos y creativos, diseñados individualmente para ti. Contamos historias significativas y construimos una comunidad fuerte alrededor de tu marca.",
+    "process.step_2.description":
+      "Desarrollamos conceptos visuales únicos y creativos, diseñados individualmente para ti. Contamos historias significativas y construimos una comunidad fuerte alrededor de tu marca.",
 
     "process.step_3.title": "3. Crear",
-    "process.step_3.description": "Ahora es mi turno. Comienzo a trabajar en tu proyecto, utilizando mi experiencia y creatividad para convertir tu visión en realidad.",
+    "process.step_3.description":
+      "Ahora es mi turno. Comienzo a trabajar en tu proyecto, utilizando mi experiencia y creatividad para convertir tu visión en realidad.",
 
     "process.step_4.title": "4. Revisar",
-    "process.step_4.description": "Recibo tus comentarios sobre el proyecto. Trabajo contigo para hacer los ajustes necesarios y asegurarme de que el resultado final cumpla con tus expectativas.",
+    "process.step_4.description":
+      "Recibo tus comentarios sobre el proyecto. Trabajo contigo para hacer los ajustes necesarios y asegurarme de que el resultado final cumpla con tus expectativas.",
 
     "process.step_5.title": "5. Entregar",
-    "process.step_5.description": "Entrego el producto final, asegurándome de que cumpla con tus expectativas y esté listo para impresionar a tu audiencia.",
+    "process.step_5.description":
+      "Entrego el producto final, asegurándome de que cumpla con tus expectativas y esté listo para impresionar a tu audiencia.",
 
     "latest_works.title": "Últimos Trabajos",
 
@@ -114,7 +145,8 @@ export const ui = {
     "portfolio.title": "Ver galería",
 
     "contact.title": "Contacta conmigo",
-    "contact.subtitle": "Tal vez aquí podamos poner un texto dando las gracias a los usuarios o cualquier mensaje que Ines quiera compartir",
+    "contact.subtitle":
+      "Tal vez aquí podamos poner un texto dando las gracias a los usuarios o cualquier mensaje que Ines quiera compartir",
     "contact.label.name": "Nombre:",
     "contact.label.email": "Correo electrónico:",
     "contact.label.message": "Mensaje:",
@@ -123,5 +155,5 @@ export const ui = {
     "map.title": "Todos mis viajes",
     "map.subtitle_1": "¡Próximamente!",
     "map.subtitle_2": "¡Países visitados!",
-  }
+  },
 } as const;
