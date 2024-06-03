000bbbf4d3a2c8bce476f7a52cecfd94cf9c59ab
Javier <javiortunoroig.com>
2024-06-03 17:20:08 +0200

    change robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,14 +3,14 @@ import type { APIRoute } from "astro";
 const robotsTxt = `
 User-agent: *
 Allow: /
-Host: https://inesworld.vercel.app/
+Host: https://inesworld.vercel.app
 Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
 
 export const GET: APIRoute = () => {
-  return new Response(robotsTxt, {
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-    },
-  });
+	return new Response(robotsTxt, {
+		headers: {
+			"Content-Type": "text/plain; charset=utf-8",
+		},
+	});
 };
