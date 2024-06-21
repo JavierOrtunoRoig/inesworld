0c8651ad184a7b858c38f2670e8cf0d4bb2b8c78
Javier <javiortunoroig.com>
2024-06-21 21:33:53 +0200

    update RSS

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,34 +1,37 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 // 2. Define tu colección(es)
 const blogsCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.date(),
-    tags: z.array(z.string()). optional(),
-    draft: z.boolean().optional().default(false),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-  })
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.date(),
+		tags: z.array(z.string()).optional(),
+		draft: z.boolean().optional().default(false),
+		image: z.object({
+			src: z.string(),
+			publicSrc: z.string(),
+			alt: z.string(),
+		}),
+	}),
 });
 
 // define a collection to store the data about a photo gallery with a title and a list of photos
 const photoGalleryCollection = defineCollection({
-  type: 'data',
-  schema: z.object({
-    title: z.string(),
-    photos: z.array(z.object({
-      path: z.string(),
-      description: z.string(),
-    })),
-  })
+	type: "data",
+	schema: z.object({
+		title: z.string(),
+		photos: z.array(
+			z.object({
+				path: z.string(),
+				description: z.string(),
+			})
+		),
+	}),
 });
 
 export const collections = {
-  'blogs': blogsCollection,
-  'gallery': photoGalleryCollection,
+	blogs: blogsCollection,
+	gallery: photoGalleryCollection,
 };
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -20,10 +20,12 @@ export async function GET(context) {
 		// Usa el "site" desde el contexto del endpoint
 		// https://docs.astro.build/en/reference/api-reference/#contextsite
 		site: `${context.site}/blog`,
+
 		// Array de `<item>`s en el xml generado
 		// Consulta la sección "Generando `items`" para ejemplos utilizando colecciones de contenido y glob imports
 		items: blog.map((post) => ({
 			title: post.data.title,
+			guid: post.id,
 			pubDate: post.data.pubDate,
 			description: post.data.description,
 			content: sanitizeHtml(parser.render(post.body), {
@@ -34,21 +36,26 @@ export async function GET(context) {
 			// This example assumes all posts are rendered as `/blog/[slug]` routes
 			link: `/posts/${post.slug}/`,
 			customData: `
-        <media:content url="https://inesworld.vercel.app/ines.webp" type="image/webp" medium="image">
+        <media:content url="${context.site}${post.data.image.publicSrc}" type="image/${post.data.image.publicSrc}" medium="image">
           <media:credit>Ines Cuevas Jiménez</media:credit>
           <media:title>${post.data.image.alt}</media:title>
           <media:text>${post.data.image.alt}</media:text>
           <media:description>${post.data.image.alt}</media:description>
         </media:content>
+        <media:thumbnail url="${context.site}${post.data.image.publicSrc}" />
+        <author>Inés Cuevas Jiménez | inesworld.mov | inesworld</author>
       `,
 		})), // (opcional) inyecta xml personalizado
 		// image should be jpg, png or gif
 		customData: `<language>en</language>
-    <image>
-      <url>https://inesworld.vercel.app/ines.jpg</url>
-      <title>Image of Ines Cuevas Jiménez | inesworld | ineworld.mov</title>
-      <link>https://inesworld.vercel.app/blog/</link>
-    </image>`,
+      <image>
+        <url>https://inesworld.vercel.app/ines.jpg</url>
+        <title>Image of Ines Cuevas Jiménez | inesworld | ineworld.mov</title>
+        <link>https://inesworld.vercel.app/blog/</link>
+      </image>
+      <copyright>Copyright © 2024 - All right reserved by Inés Cuevas Jiménez | inesworld.mov | inesworld</copyright>
+      <atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />
+		`,
 		// stylesheet: "/rss-pretty-feed-v3.xsl",
 	});
 }
