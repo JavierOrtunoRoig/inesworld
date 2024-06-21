277768eeb3b1b6ada5658f816c53ef237b6e96e5
Javier <javiortunoroig.com>
2024-06-21 16:00:41 +0200

    update RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const blog = await getCollection("blogs");
@@ -18,11 +21,9 @@ export async function GET(context) {
 			title: post.data.title,
 			pubDate: post.data.pubDate,
 			description: post.data.description,
-			enclosure: {
-				url: post.data.image.src,
-				type: "image/jpeg",
-				length: post.data.image.src.length,
-			},
+			content: sanitizeHtml(parser.render(post.body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+			}),
 			// customData: post.data.customData,
 			// Compute RSS link from post `slug`
 			// This example assumes all posts are rendered as `/blog/[slug]` routes
