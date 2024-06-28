26bfd37c2097754093691c3f94c096942346778a
Javier <javiortunoroig.com>
2024-06-28 14:03:47 +0200

    refactor blog to use tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
