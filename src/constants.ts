3cc47d4a2195fb607e0fa6d2bac1ddfa92ee944d
Javier <javiortunoroig.com>
2024-07-17 20:06:48 +0200

    remove unused trips

--- src/constants.ts
+++ src/constants.ts
@@ -52,44 +52,9 @@ export const nextTravels: Travel[] = [
 		country: "Philippines",
 		country_es: "Filipinas",
 		startDate: "2024-07-21",
-		endDate: "2024-09-29",
+		endDate: "2024-10-01",
 		coordinates: [12.879721, 121.774017],
 	},
-	{
-		country: "South Korea",
-		country_es: "Corea del Sur",
-		startDate: "2025-02-01",
-		endDate: "2025-07-15",
-		coordinates: [37.5326, 127.024612],
-	},
-	{
-		country: "Switzerland",
-		country_es: "Suiza",
-		startDate: "2024-06-20",
-		endDate: "2024-06-25",
-		coordinates: [46.818188, 8.227512],
-	},
-	{
-		country: "Barcelona",
-		country_es: "Barcelona",
-		startDate: "2024-07-02",
-		endDate: "2024-07-04",
-		coordinates: [41.38879, 2.15899],
-	},
-	{
-		country: "Toulouse",
-		country_es: "Toulouse",
-		startDate: "2024-07-05",
-		endDate: "2024-07-05",
-		coordinates: [43.60426, 1.44367]
-	},
-	{
-		country: "Pyrenees",
-		country_es: "Pirineos",
-		startDate: "2024-07-06",
-		endDate: "2024-07-09",
-		coordinates: [42.666667, 1.6679]
-	}
 ];
 
 const MALAGA = [36.72016, -4.42034];
