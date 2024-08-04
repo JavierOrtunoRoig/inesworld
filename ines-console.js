f079578d86650019de4c59d2244ac2607c739def
Javier <javiortunoroig.com>
2024-08-04 17:07:15 +0200

    Update translations

--- ines-console.js
+++ ines-console.js
@@ -1,16 +1,25 @@
-import { intro, log, outro, select, text,  } from '@clack/prompts';
+import { intro, log, outro, select, text, cancel } from '@clack/prompts';
 import en from './src/en.json' assert { type: 'json' };
 import es from './src/es.json' assert { type: 'json' };
 import fs from 'fs';
+import { exec } from 'child_process';
 
 const actions = {
   cambiarTexto,
 }
 
+const yesNoOptions = [
+  {
+    value: 'yes',
+    label: 'Sí',
+  },
+  {
+    value: 'no',
+    label: 'No',
+  }
+]
 
-intro(`¡Hey, it's Javi! Este es el panel de administración de InesWorld. ¿En qué puedo ayudarte?`);
-
-const projectType = await select({
+const selectAction = {
   message: 'Elige la acción que quieras realizar',
   options: [
     {
@@ -19,18 +28,24 @@ const projectType = await select({
       hint: 'Actualiza los textos de la web',
     },
   ],
-});
+}
+
+intro(`¡Hey, it's Javi! Este es el panel de administración de InesWorld. ¿En qué puedo ayudarte?`);
+
+const projectType = await select(selectAction);
 
 await actions[projectType]();
 
 
-outro(`Enohorabuena, has actulizado la web de InesWorld. ¡A seguir creando, hasta la próxima!`);
+outro(`Enhorabuena, has actulizado la web de InesWorld. ¡A seguir creando, hasta la próxima!`);
 
+/**
+ * Get the desired language from the user and the not desired language
+ * 
+ * @returns {Promise<{ desiredLanguage: string, notDesiredLanguage: string }>}
+ */
 
-async function cambiarTexto() {
-
-  log.info('Vamos a cambiar el texto de la web. Para esto primero necesito saber qué texto quieres cambiar. Voy a buscar entre todos los textos de la web');
-
+async function getLanguages() {
   const desiredLanguage = await select({
     message: '¿En qué idioma quieres buscar el texto?',
     options: [
@@ -47,17 +62,45 @@ async function cambiarTexto() {
 
   const notDesiredLanguage = desiredLanguage === 'en' ? 'es' : 'en';
 
-  const dictionary = desiredLanguage === 'en' ? en : es;
+  return { desiredLanguage, notDesiredLanguage };
+}
+
+/**
+ * @param {string} desiredLanguage
+ * @returns {Promise<{ desiredDictionary notDesiredDictionary }>}
+ */
+async function getDictionaries(desiredLanguage) {
+  return {
+    desiredDictionary: desiredLanguage === 'en' ? en : es,
+    notDesiredDictionary: desiredLanguage === 'en' ? es : en,
+  }
+}
+
+
+async function cambiarTexto() {
+
+  log.info('Vamos a cambiar el texto de la web. Para esto primero necesito saber qué texto quieres cambiar. Voy a buscar entre todos los textos de la web');
+
+  
+  const { desiredLanguage, notDesiredLanguage } = await getLanguages();
+
+  const { desiredDictionary, notDesiredDictionary } = await getDictionaries(desiredLanguage);
 
   const desiredText = await text({
     message: '¿Podrías escribir parte del texto actual que quieres cambiar? Ya sea una palabra o una frase:',
     hint: `Por ejemplo: "Hey, it's Ines"`,
   });
 
-  log.info(`Buscando textos que contengan "${desiredText}" en el idioma ${desiredLanguage}...`);
+  log.info(`Buscando textos que contengan "${desiredText}" en el idioma '${desiredLanguage}'...`);
 
   // get an array of object with the keys that match the desired text and their values
-  const matchingTexts = Object.keys(dictionary).filter(key => dictionary[key].toLowerCase().includes(desiredText.toLowerCase())).map(key => ({ key, value: dictionary[key] }));
+  const matchingTexts = Object.keys(desiredDictionary).filter(key => desiredDictionary[key].toLowerCase().includes(desiredText.toLowerCase())).map(key => ({ key, value: desiredDictionary[key] }));
+
+  if (matchingTexts.length === 0) {
+    log.error('No se han encontrado textos que coincidan con la búsqueda');
+    outro('Vuelve a iniciar el programa para intentarlo de nuevo');
+    process.exit(1);
+  }
 
   // show a select with the matching texts
   const selectedKey = await select({
@@ -73,16 +116,7 @@ async function cambiarTexto() {
 
   const updateOtherLanguage = await select({
     message: `¿Quieres proporcionar el nuevo texto para el idioma '${notDesiredLanguage}'?`,
-    options: [
-      {
-        value: 'yes',
-        label: 'Sí',
-      },
-      {
-        value: 'no',
-        label: 'No',
-      }
-    ],
+    options: yesNoOptions,
   });
 
   if (updateOtherLanguage === 'yes') {
@@ -91,37 +125,26 @@ async function cambiarTexto() {
     });
   }
 
-  // show info about the changes. If english dictionary or spanish dictionary or both will be updated and ask for confirmation
   log.message('Vas a cambiar el siguiente texto:');
-  log.message(`"${dictionary[selectedKey]}"`);
+  log.message(`"${desiredDictionary[selectedKey]}"`);
   log.message('Por:');
   log.message(`"${newTraduction}"`);
   log.message(`En el idioma '${desiredLanguage}'`);
 
   if (updateOtherLanguage === 'yes') {
-    log.info(`Además, vas a cambiar el texto "${dictionary[selectedKey]}" por "${newTraductionInOtherLanguage}" en el idioma ${notDesiredLanguage}`);
+    log.info(`Además, vas a cambiar el texto "${desiredDictionary[selectedKey]}" por "${newTraductionInOtherLanguage}" en el idioma ${notDesiredLanguage}`);
   }
 
   const confirmChanges = await select({
     message: '¿Quieres aplicar estos cambios?',
-    options: [
-      {
-        value: 'yes',
-        label: 'Sí',
-      },
-      {
-        value: 'no',
-        label: 'No',
-      }
-    ],
+    options: yesNoOptions,
   });
 
   if (confirmChanges === 'yes') {
-    dictionary[selectedKey] = newTraduction;
+    desiredDictionary[selectedKey] = newTraduction;
 
     if (updateOtherLanguage === 'yes') {
-      const otherLanguageDictionary = notDesiredLanguage === 'en' ? en : es;
-      otherLanguageDictionary[selectedKey] = newTraductionInOtherLanguage;
+      notDesiredDictionary[selectedKey] = newTraductionInOtherLanguage;
     }
 
   } else {
@@ -129,18 +152,33 @@ async function cambiarTexto() {
   }
 
   // save the changes overwriting the file
-  const dictionaryToSave = desiredLanguage === 'en' ? en : es;
   const dictionaryPath = desiredLanguage === 'en' ? './src/en.json' : './src/es.json';
 
-  fs.writeFileSync(dictionaryPath, JSON.stringify(dictionaryToSave, null, 2));
+  fs.writeFileSync(dictionaryPath, JSON.stringify(desiredDictionary, null, 2));
 
   if (updateOtherLanguage === 'yes') {
-    const otherLanguageDictionaryToSave = notDesiredLanguage === 'en' ? en : es;
     const otherLanguageDictionaryPath = notDesiredLanguage === 'en' ? './src/en.json' : './src/es.json';
 
-    fs.writeFileSync(otherLanguageDictionaryPath, JSON.stringify(otherLanguageDictionaryToSave, null, 2));
+    fs.writeFileSync(otherLanguageDictionaryPath, JSON.stringify(notDesiredDictionary, null, 2));
   }
 
   log.success('Cambios guardados con éxito');
+
+  log.info('Intentando subir los cambios de la web...');
+
+  try {
+    exec('git add . && git commit -m "Update translations" && git push', (error, stdout, stderr) => {
+      if (error) {
+        log.error('No se ha podido subir los cambios a la web');
+        log.error(error.message);
+        return;
+      }
+
+      log.success('Cambios subidos a la web con éxito');
+    });
+  } catch (error) {
+    log.error('No se ha podido subir los cambios a la web');
+    log.error(error.message);
+  }
 
 }
